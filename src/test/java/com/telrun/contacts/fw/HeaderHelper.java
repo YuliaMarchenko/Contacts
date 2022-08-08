f4db5a04415ecc1aa2c0ee3228d42cc72c2016d5
Yulia Marchenko <yuyakovleva11@gmail.com>
2022-08-08 22:45:05 +0200

    hm

--- src/test/java/com/telrun/contacts/fw/HeaderHelper.java
+++ src/test/java/com/telrun/contacts/fw/HeaderHelper.java
@@ -3,7 +3,7 @@ package com.telrun.contacts.fw;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class HeaderHelper extends HelperBase{
+public class HeaderHelper extends HelperBase {
     public HeaderHelper(WebDriver wd) {
         super(wd);
     }
@@ -19,4 +19,21 @@ public class HeaderHelper extends HelperBase{
     public boolean isSignOutPresent() {
         return isElementPresent(By.xpath("//button[contains(.,'Sign Out')]"));
     }
+
+    public boolean isHomePresent() {
+        return isElementPresent(By.xpath("//a[contains(.,'HOME')]"));
+    }
+
+    public boolean isAboutPresent() {
+        return isElementPresent(By.xpath("//a[contains(.,'ABOUT')]"));
+    }
+
+    public boolean isContactPresent() {
+        return isElementPresent(By.xpath("//a[contains(.,'CONTACT')]"));
+    }
+
+    public boolean isAddPresent() {
+        return isElementPresent(By.xpath("//a[contains(.,'ADD')]"));
+    }
+
 }
--- src/test/java/com/telrun/contacts/tests/HomePageTests.java
+++ src/test/java/com/telrun/contacts/tests/HomePageTests.java
@@ -1,10 +1,27 @@
 package com.telrun.contacts.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HomePageTests extends TestBase{
     @Test
     public void testOpenHomePage() {
         app.getContact().isComponentFormPresent();
+    }
+
+    @Test
+    public void checkHeaderWithoutLogin(){
+        Assert.assertTrue(app.getHeader().isHomePresent());
+        Assert.assertTrue(app.getHeader().isAboutPresent());
+        Assert.assertTrue(app.getHeader().isLoginLinkPresent());
+    }
+
+    @Test
+    public void checkHeaderWithLogin(){
+        app.getUser().login();
+        Assert.assertTrue(app.getHeader().isHomePresent());
+        Assert.assertTrue(app.getHeader().isAboutPresent());
+        Assert.assertTrue(app.getHeader().isContactPresent());
+        Assert.assertTrue(app.getHeader().isAddPresent());
     }
 }
